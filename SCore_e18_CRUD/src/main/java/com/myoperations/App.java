c5177657107248e29d47130e6288591feea56d71
Deepti Mahisare <deeptimahisare@gmail.com>
2024-11-23 16:10:32 +0530

    update

--- SCore_e18_CRUD/src/main/java/com/myoperations/App.java
+++ SCore_e18_CRUD/src/main/java/com/myoperations/App.java
@@ -15,11 +15,34 @@ public class App
         ApplicationContext ac = new ClassPathXmlApplicationContext("config.xml");
         JdbcTemplate jt = (JdbcTemplate)ac.getBean("my_jdbc",JdbcTemplate.class);
 //        System.out.println(jt);
+
+//      =========================================================================================
+        //Insert
+//        String query = "insert into Student(sname,percent) values(?,?)";
+//        int i =jt.update(query,"Ceep",97.9);
+//        System.out.println("Inserted Successfully!!" + i);
+//        =========================================================================================
         
+        //Delete
+//        String query = "delete from Student where sid = ?";
+//        int i = jt.update(query, 4);
+//      System.out.println("Deleted Successfully!!" + i);
         
-        String query = "insert into Student(sname,percent) values(?,?)";
-        int i =jt.update(query,"Ceep",97.9);
-        System.out.println("Inserted Successfully!!" + i);
+//      =========================================================================================
         
+
+        //Update
+//        String query = "update student set sname=? where sid = ?";
+//        int i = jt.update(query, "Heep",2);
+//      System.out.println("Updated Successfully!!" + i);
+      
+//      =========================================================================================
+        
+        //Select one Entry
+        String query = "select * from Student where sid =?";
+        Student i = jt.queryForObject(query, new RowMapperImpl(),2);
+
+      System.out.println("Roll no.: "+i.getRoll()+"\nName: "+i.getName()+"\nPercentage: "+i.getPercent());
+//      =========================================================================================
     }
 }
--- SCore_e18_CRUD/src/main/java/com/myoperations/RowMapperImpl.java
+++ SCore_e18_CRUD/src/main/java/com/myoperations/RowMapperImpl.java
@@ -1,6 +1,20 @@
 package com.myoperations;
 
-public class RowMapperImpl  {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class RowMapperImpl implements RowMapper<Student> {
+
+	@Override
+	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Student st = new Student();
+		st.setRoll(rs.getInt("sid"));
+		st.setName(rs.getString(2));
+		st.setPercent(rs.getFloat("percent"));
+		return st;
+	}
 	
 	
 
